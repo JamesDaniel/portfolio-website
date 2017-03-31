e85b972f2df1e052ee284b209d1b42b0500c9de1
James <james.d.mcgarr@students.ittralee.ie>
2017-03-31 22:24:59 +0000

    workaround for adding sayings string to UI with in angular app

--- public/app/app.js
+++ public/app/app.js
@@ -2,5 +2,5 @@ angular.module('app', ['ngResource']);
 
 angular.module('app').controller('sayingsCtrl', function($scope, $resource) {
     $scope.quote = '\"Measuring programming progress by lines of code is like measuring aircraft building progress by weight\" ~Bill Gates';
-    $scope.quote = $resource('/api/sayings/first').query();
+    //$scope.quote = $resource('/api/sayings/first').query();
 });
